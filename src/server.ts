65c054dfa6693d4dd49e193105900ec68606b213
Adin Silajdžić <asilajdzic2000@gmail.com>
2024-06-28 23:41:41 +0200

    added server, db

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import { getPayloadClient } from './get-payoad';
+import {
+  nextApp,
+  nextHandler,
+} from './next-utils';
+import _ from 'lodash';
+
+const app = express();
+const PORT =
+  Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const payload =
+    await getPayloadClient({
+      initOptions: {
+        express: app,
+        onInit: async (cms) => {
+          cms.logger.info(
+            `Admin URL ${cms.getAdminURL()}`
+          );
+        },
+      },
+    });
+  app.use((req, res) =>
+    nextHandler(req, res)
+  );
+
+  nextApp.prepare().then(() => {
+    payload.logger.info(
+      'Next.js started'
+    );
+  });
+
+  app.listen(PORT, async () => {
+    payload.logger.info(
+      `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+    );
+  });
+};
+
+start();
